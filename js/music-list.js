92387db48cdf2e14044062f590d6cd190f2bfe16
Jihann <1244098192@qq.com>
2015-09-20 15:08:45 +0800

    添加新歌曲，播放顺序

--- js/music-list.js
+++ js/music-list.js
@@ -4,15 +4,51 @@
 var musicList = [
     {
         star : '梁静茹',
-        name : 'See you again',
-        url : 'media/See You Again.mp3',
+        name : '会呼吸的痛',
+        url : 'media/会呼吸的痛.mp3',
         img : 'images/music/star/ljr.jpg'
     },
     {
-        star : '王力宏',
-        name : '往事如风',
-        url : 'media/往事如风-杜舞雩.mp3',
-        img : 'images/music/star/wlh.jpg'
+        star : '维兹·卡利法',
+        name : 'See you again',
+        url : 'media/See You Again.mp3',
+        img : 'images/music/star/su7.png'
+    },
+    {
+        star : '徐良',
+        name : '七秒钟的记忆',
+        url : 'media/七秒钟的记忆.mp3',
+        img : 'images/music/star/xl.png'
+    },
+    {
+        star : '徐良',
+        name : '后会无期',
+        url : 'media/后会无期.mp3',
+        img : 'images/music/star/xl.png'
+    },
+    {
+        star : '徐良',
+        name : '坏女孩',
+        url : 'media/坏女孩.mp3',
+        img : 'images/music/star/xl.png'
+    },
+    {
+        star : '徐良',
+        name : '客官不可以',
+        url : 'media/客官不可以.mp3',
+        img : 'images/music/star/xl.png'
+    },
+    {
+        star : '徐良',
+        name : '虐心',
+        url : 'media/虐心.mp3',
+        img : 'images/music/star/xl.png'
+    },
+    {
+        star : '徐良',
+        name : '犯贱',
+        url : 'media/犯贱.mp3',
+        img : 'images/music/star/xl.png'
     }
 
 ];
--- js/music.js
+++ js/music.js
@@ -1,50 +1,125 @@
 /**
  * Created by Jihann on 2015/9/19.
  */
-;(function($) {
+;(function($, window, document, undefined) {
     var MusicPlayer = function() {
         var _this = this;
-
+        this.document = document;
         this.index = 1;
+        this.currentIndex = -1;
         this.musicList = musicList;
         this.musicAudio = null;
+
         this.music = $(".music-wrapper");
         this.starImg = this.music.find("div.star-img img");
         this.starName = this.music.find("div.star-info");
         this.musicName = this.music.find("span.music-name");
         this.audio = this.music.find("audio");
         this.btnPlay = this.music.find("div.btn-play");
+        this.btnPrev = this.music.find("div.btn-prev");
+        this.btnNext = this.music.find("div.btn-next");
+        this.btnAma = this.music.find(".icon-ama");
+        this.btnRandom = this.music.find(".icon-ran");
+        this.btnList = this.music.find("div.icon-list");
 
-        _this.init();
+        _this.init(_this.index);
+
         this.btnPlay.click(function() {
             if (_this.btnPlay.hasClass("play")) {
                 _this.play("play");
-                _this.btnPlay.removeClass("play").addClass("pause").text("=");
+                _this.selectClass("play");
             } else {
                 _this.play("pause");
-                _this.btnPlay.removeClass("pause").addClass("play").text("+");
+                _this.selectClass("pause");
             }
         });
+
+        this.btnPrev.click(function() {
+            _this.play("prev");
+        });
+
+        this.btnNext.click(function() {
+            _this.play("next");
+        });
+
+        this.btnAma.click(function() {
+            var self = $(this);
+            if (self.hasClass("icon-ama-click")) {
+                self.removeClass("icon-ama-click");
+            } else {
+                self.addClass("icon-ama-click");
+            }
+        });
+
+        this.btnRandom.click(function() {
+            var index = parseInt(_this.musicList.length * Math.random(), 10);
+            if (index === _this.currentIndex) {
+                index += 1;
+            }
+            _this.init(index); //通过随机数获取当前数据中的数据
+            _this.currentIndex = index; //保存当前索引，由下面左右键自行加减
+        });
+
+        this.btnList.click(function() {
+            _this.music.fadeOut();
+        });
+
+        this.musicAudio.addEventListener("ended", function() {
+           _this.play("next");
+        }, false);
     };
     MusicPlayer.prototype = {
-        init : function() {
-            var _this = this;
-            this.musicAudio = this.audio.get(0);
-            this.starImg.attr("src", this.musicList[this.index]["img"]);
-            this.starName.text(this.musicList[this.index]["star"]);
-            this.musicName.text(this.musicList[this.index]["name"]);
-            this.audio.attr("src", this.musicList[this.index]["url"]);
-
+        init : function(index) {
+            var self = this;
+            self.musicAudio = self.audio.get(0);
+            self.musicAudio.volume = 0.2;
+            self.starImg.attr("src", self.musicList[index]["img"]);
+            self.starName.text(self.musicList[index]["star"]);
+            self.musicName.text(self.musicList[index]["name"]);
+            self.audio.attr("src", self.musicList[index]["url"]);
+            self.musicAudio.play();
+            this.selectClass("play");
         },
         play : function(status) {
             var _this = this;
             if (status === "play") {
                 _this.musicAudio.play();
-            } else {
+            } else if (status === "pause") {
                 _this.musicAudio.pause();
+            } else if (status === "prev") {
+                this.change("prev");
+            } else if (status === "next") {
+                this.change("next");
+            }
+        },
+        change : function(status) {
+            var len = this.musicList.length;
+            if (status === "prev") {
+                if (this.currentIndex === -1) {
+                    this.currentIndex = 0;
+                } else if (this.currentIndex === 0) {
+                    this.currentIndex = len - 1;
+                } else {
+                    this.currentIndex--;
+                }
+            } else if (status === "next") {
+                if (this.currentIndex === -1) {
+                    this.currentIndex = 0 + 2; //我是默认加载第2条数据
+                } else if (this.currentIndex === (len - 1)) {
+                    this.currentIndex = 0;
+                } else {
+                    this.currentIndex++;
+                }
+            }
+            this.init(this.currentIndex);
+        },
+        selectClass : function(play) {
+            if (play === "play") {
+                this.btnPlay.removeClass("play").addClass("pause").text("=");
+            } else if (play === "pause") {
+                this.btnPlay.removeClass("pause").addClass("play").text("+");
             }
         }
     };
-
     window["MusicPlayer"] = MusicPlayer;
-})(jQuery);
+})(jQuery, window, document);
